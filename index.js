e1e2a11700b33f55578ba3b0205d0e4e3d648cf4
Ankit <ankitmittal0814@gmail.com>
2025-07-10 14:32:20 +0530

    Update form design and layout

--- index.js
+++ index.js
@@ -3,28 +3,25 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const pdf = require('html-pdf');
 const path = require('path');
-
+const PORT = 4000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-// Route to show the form
 app.get('/', (req, res) => {
     res.render('form');
 });
 
-// Route to handle form submission and generate PDF
+
 app.post('/generate-pdf', (req, res) => {
     const formData = req.body;
 
-    // Render the template with form data
     ejs.renderFile(path.join(__dirname, 'views/template.ejs'), formData, (err, html) => {
         if (err) {
             return res.status(500).send('Template rendering error');
         }
 
-        // Create PDF
         pdf.create(html).toBuffer((err, buffer) => {
             if (err) {
                 return res.status(500).send('PDF generation failed');
@@ -37,6 +34,6 @@ app.post('/generate-pdf', (req, res) => {
     });
 });
 
-app.listen(4000, () => {
-    console.log('Server started on http://localhost:3000');
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
 });
